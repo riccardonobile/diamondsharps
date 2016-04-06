000dd89d07dd36b21def72e7377b854cadd7fd5f
Simone Cragnolini <simone.cragnolini@gmail.com>
2016-04-06 12:00:05 +0200

    Added initial files for client app "InCaneva"

---
+++ Gruppo 2/InCaneva/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inCanevaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the inCanevaApp
+ */
+angular.module('inCanevaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Gruppo 2/InCaneva/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inCanevaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inCanevaApp
+ */
+angular.module('inCanevaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
