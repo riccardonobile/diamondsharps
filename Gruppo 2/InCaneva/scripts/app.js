c0180f49a688fbc8201f841c2263cc48fa7f8f7c
Simone Cragnolini <simone.cragnolini@gmail.com>
2016-04-07 16:16:00 +0200

    I've worked on something

--- Gruppo 2/InCaneva/scripts/app.js
+++ Gruppo 2/InCaneva/scripts/app.js
@@ -6,19 +6,26 @@ var app = angular.module('incanevaApp', []);
 
 app.controller('main', function ($scope, $http) {
     
+    var json;
+    
     $scope.loadData = function (blog, old, limit, offset, filter) {
 
         var postString = "action=incaneva_events&blog="+blog+"&old="+old+"&limit="+limit+"&offset="+offset+"&filter="+filter;
 
-        $http.post("http://incaneva.it/wp-admin/admin-ajax.php",postString,{
+        $http.post("http://incaneva.it/wp-admin/admin-ajax.php", postString, {
             headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
         })
             .then(function (result) {
-                var json = result.data.data;
-                console.log(json);
+                json = result.data.data;
             });
 
     };
+    
+    $scope.filterByDate = function (month) {
+        json.forEach(function (el) {
+            if(el.post_month_numerical)
+        })
+    }
     
     $scope.loadData("1,6,7,8", "", "", "", "");
 
