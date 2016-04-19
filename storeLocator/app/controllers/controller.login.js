63f4a5443ea820a87403caa4bc5289790466b1ca
Riccardo Nobile <riccardo.nobile@winfuture.it>
2016-04-19 21:44:44 +0200

    Delete button for going to login page in the index and add control if user is loggeg in for redirect to homepage in loginController

--- storeLocator/app/controllers/controller.login.js
+++ storeLocator/app/controllers/controller.login.js
@@ -1,4 +1,7 @@
 storeLocator.controller('loginController', ['$scope', '$rootScope', '$location', 'authManager', 'storageManager', function($scope, $rootScope, $location, authManager, storageManager) {
+    if (storageManager.getSession() !== null) {
+        $location.url('/');
+    }
     $scope.login = function() {
         authManager.login($scope.user.email, $scope.user.password, function(err, resp) {
             if(!err) {
