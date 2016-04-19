575571b4701a8523ac1bcb8fd975380c8e8bca6b
Riccardo Nobile <riccardo.nobile@winfuture.it>
2016-04-19 21:19:20 +0200

    Added sessionManager factory for session validity check

---
+++ storeLocator/app/factories/factory.sessionManager.js
@@ -0,0 +1,39 @@
+storeLocator.factory('sessionManager', ['$http', '$httpParamSerializerJQLike', 'appConfig', 'storageManager',function($http, $httpParamSerializerJQLike, appConfig, storageManager) {
+    var session_manager = {};
+
+    session_manager.verify = function(completionHandler) {
+
+        var _session = storageManager.getSession();
+        if(_session) {
+            _session = _session.session;
+            $http.get(appConfig.sessionUrl + _session, $httpParamSerializerJQLike({session: _session}), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).success(function(res) {
+                if(res.success) {
+                    completionHandler(null, (res.data||[]));
+                } else {
+                    completionHandler(true, res);
+                }
+            }).error(function() {
+                completionHandler(true, {
+                    errorCode: 0,
+                    errorMessage: 'Error on login request'
+                });
+            });
+        } else {
+            completionHandler(true, {
+                errorCode: 0,
+                errorMessage: 'Error on login request'
+            });
+        }
+    };
+
+    session_manager.logout = function(completionHandler) {
+        completionHandler(null, true);
+    };
+
+    return session_manager;
+}]);
+
