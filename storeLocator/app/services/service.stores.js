4a1016372c1aa36ab28bfdabfdc14a8b3d704eed
Riccardo Nobile <riccardo.nobile@winfuture.it>
2016-04-20 12:11:50 +0200

    Add storesService

---
+++ storeLocator/app/services/service.stores.js
@@ -0,0 +1,22 @@
+storeLocator.service('storesService', [ function() {
+    this.stores = {};
+
+    this.set = function(obj) {
+        this.stores = obj;
+    };
+
+    this.get = function() {
+        if(this.isValid()) {
+            return this.stores;
+        }
+        return undefined;
+    };
+
+    this.isValid = function() {
+        return ((this.stores !== undefined) && (_.key(this.stores)||[]).length > 0);
+    };
+
+    this.clear = function() {
+        this.stores = {};
+    }
+}]);
