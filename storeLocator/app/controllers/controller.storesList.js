1ba12b0b2855d873b27eddf4ae4137eb660d6356
Simone Cragnolini <simone.cragnolini@gmail.com>
2016-04-26 09:59:14 +0200

    Added compute of distance

--- storeLocator/app/controllers/controller.storesList.js
+++ storeLocator/app/controllers/controller.storesList.js
@@ -1,7 +1,26 @@
-storeLocator.controller('storesListController', ['$scope', 'storeManager', function($scope, storeManager) {
+storeLocator.controller('storesListController', ['$scope', 'storeManager', '$geolocation', 'mapManager', function($scope, storeManager, $geolocation, mapManager) {
     storeManager.getAll(function(err, resp) {
         if(!err) {
-            $scope.stores = resp;
+            $geolocation.getCurrentPosition({
+                timeout: 60000
+            }).then(function(position) {
+                var myCoords = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var newResp = [];
+                resp.forEach(function(el) {
+                    el.distance = parseInt(mapManager.getDistance(
+                        myCoords,
+                        {
+                            lat: el.latitude,
+                            lng: el.longitude
+                        }
+                    ));
+                    newResp.push(el);
+                });
+                $scope.stores = newResp;
+            });
         }
     });
 }]);
--- storeLocator/app/factories/factory.mapManager.js
+++ storeLocator/app/factories/factory.mapManager.js
@@ -46,5 +46,27 @@ storeLocator.factory('mapManager', ['appConfig', function(appConfig) {
         map_manager.map.setOptions(map_manager.config);
     };
 
+    map_manager.getDistance = function(position1, position2) {
+        function toRad(degrees) {
+            return degrees * Math.PI / 180
+        }
+
+        var lat1 = position1.lat;
+        var lon1 = position1.lng;
+        var lat2 = position2.lat;
+        var lon2 = position2.lng;
+
+        var R = 6371; // km
+        var dLat = toRad(lat2-lat1);
+        var dLon = toRad(lon2-lon1);
+        var lat1 = toRad(lat1);
+        var lat2 = toRad(lat2);
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return R * c;
+    };
+
     return map_manager;
 }]);
