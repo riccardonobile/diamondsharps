8511b206342227ac64b804aa277d92f2f2bbe09c
ClaireFrancoise <formentin.claire@gmail.com>
2016-05-09 15:41:14 +0200

    Used https instead of http as protocol for api endpoints

--- storeLocator/app/app.constants.js
+++ storeLocator/app/app.constants.js
@@ -1,5 +1,5 @@
 storeLocator.constant('appConfig', {
-    'authUrl': 'http://its-bitrace.herokuapp.com/api/public/v2/login',
-    'apiUrl': 'http://its-bitrace.herokuapp.com/api/v2/stores',
-    'sessionUrl': 'http://its-bitrace.herokuapp.com/api/v2/session/'
+    'authUrl': 'https://its-bitrace.herokuapp.com/api/public/v2/login',
+    'apiUrl': 'https://its-bitrace.herokuapp.com/api/v2/stores',
+    'sessionUrl': 'https://its-bitrace.herokuapp.com/api/v2/session/'
 });
